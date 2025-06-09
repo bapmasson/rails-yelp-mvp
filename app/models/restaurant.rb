955d52c31f32a91b51991509d61b618d9c4da09f
Baptiste Masson <baptiste.masson@hotmail.fr>
2025-06-09 21:00:27 +0200

    added navbar and completed all restaurant routes for user stories

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,5 +3,5 @@ class Restaurant < ApplicationRecord
 
   validates :name, :address, :category, presence: true
   validates :category, inclusion: { in: %w(chinese italian japanese french belgian),
-    message: "%{value} is not a valid category" }
+    message: "%{value} is not a valid category. Please choose from chinese, italian, japanese, french, belgian." }
 end
