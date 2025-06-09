fc675af3b73320756b247fbe2734063532a14d90
Baptiste Masson <baptiste.masson@hotmail.fr>
2025-06-09 21:59:23 +0200

    add reviews routes and all working just need some style and buttons + refacto as asked

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,5 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
   validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: %w(chinese italian japanese french belgian),
-    message: "%{value} is not a valid category. Please choose from chinese, italian, japanese, french, belgian." }
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
 end
